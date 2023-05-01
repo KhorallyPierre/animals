490558ed9891ef0e4cce5d161642cee21cc9c25a
Khorally-s <kvpierre@gmail.com>
2023-05-01 13:23:51 -0400

    css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {useState} from 'react';
+import './app.css';
+import { useState } from 'react';
 import AnimalShow from './AnimalShow';
+
 
 
 function getRandomAnimal() {
@@ -8,24 +10,25 @@ function getRandomAnimal() {
     return animals[Math.floor(Math.random() * animals.length)]
 }
 
-function App(){
+function App() {
     const [animals, setAnimals] = useState([])
-  
+
     // this is an eventhandler for when user clicks on something. 
     const handleClick = () => {
-        
-     setAnimals([...animals, getRandomAnimal()])
+
+        setAnimals([...animals, getRandomAnimal()])
     };
 
     const renderedAnimals = animals.map((animal, index) => {
-        return <AnimalShow type={animal} key={index}/>
+        return <AnimalShow type={animal} key={index} />
     })
-    return  <div> 
-        {/* onClick is the event */}
-        <button onClick={handleClick}> Add animal</button>
-        <div> {renderedAnimals}</div>
-    </div>
-    
+    return (
+        <div className='app'>
+            {/* onClick is the event */}
+            <button onClick={handleClick}> Add animal</button>
+            <div className="animal-list"> {renderedAnimals}</div>
+        </div>
+    );
 }
 
 export default App

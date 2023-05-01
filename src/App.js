4fc527024a62d97db585f22a4c01e2ffaed19cc1
Khorally-s <kvpierre@gmail.com>
2023-05-01 11:23:34 -0400

    mapping over animals and printing as a new array through AnimalShow component; spread syntax in setAnimals (building out the array of animals during handleClick)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App(){
+    const [animals, setAnimals] = useState([])
+  
     // this is an eventhandler for when user clicks on something. 
-    //it's a callback function (the purpose of this function is to be called 
-    //automatically by some other piece of code 
     const handleClick = () => {
-      console.log('the mouse mocws')  
+        
+     setAnimals([...animals, getRandomAnimal()])
     };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index}/>
+    })
     return  <div> 
         {/* onClick is the event */}
         <button onClick={handleClick}> Add animal</button>
+        <div> {renderedAnimals}</div>
     </div>
     
 }
